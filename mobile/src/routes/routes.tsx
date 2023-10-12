dcb62c2b2d00e3b731d2648eb4e7a994d2982f1a
Anderson Silva <andersonnsilva@alu.ufc.br>
2023-10-12 20:26:08 -0300

    Creating Home Screen

---
+++ mobile/src/routes/routes.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from '../screens/Home';
+import MonitorScreen from '../screens/Monitor';
+
+const Stack = createNativeStackNavigator();
+
+function Routes() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen name="Monitor" component={MonitorScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default Routes;
