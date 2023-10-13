7412c97284d734c423bdd4d8ac546f54295b8535
Anderson Silva <andersonnsilva@alu.ufc.br>
2023-10-13 08:04:54 -0300

    Creating History Screen (#2)
    * Creating History Screen
    
    * Removing WrapperIcon

--- mobile/src/routes/routes.tsx
+++ mobile/src/routes/routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../screens/Home';
 import MonitorScreen from '../screens/Monitor';
+import HistoryScreen from '../screens/History';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,13 @@ function Routes() {
       <Stack.Screen
         name="Home"
         component={HomeScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="History"
+        component={HistoryScreen}
         options={{
           headerShown: false,
         }}
---
+++ mobile/src/screens/History.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {StyleSheet, View, Text, FlatList, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+interface DateTimeItem {
+  id: string;
+  dateTime: string;
+  status: 'success' | 'failure';
+}
+
+interface HistoryScreenProps extends NativeStackScreenProps<any, 'History'> {}
+
+function HistoryScreen({navigation}: HistoryScreenProps): JSX.Element {
+  const mock: DateTimeItem[] = [
+    {
+      id: '1',
+      dateTime: '26/09/2023 - 16:00',
+      status: 'success',
+    },
+    {
+      id: '2',
+      dateTime: '26/09/2023 - 16:00',
+      status: 'failure',
+    },
+    {
+      id: '3',
+      dateTime: '26/09/2023 - 16:00',
+      status: 'failure',
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.wrapperIcon}
+        onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={24} color="#fff" />
+      </TouchableOpacity>
+      <Text style={styles.title}>HISTÓRICO DE ACESSOS</Text>
+      <Text style={styles.description}>
+        Aqui você acompanha todas as movimentações que ocorreram na tranca
+      </Text>
+      <FlatList
+        style={styles.scrollViewContainer}
+        data={mock}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <View style={styles[item.status]}>
+            <Text style={styles.dateTimeText}>{item.dateTime}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 32,
+    marginTop: 64,
+  },
+  wrapperIcon: {
+    backgroundColor: 'transparent',
+    marginBottom: 26,
+  },
+  title: {
+    fontSize: 32,
+    fontFamily: 'Poppins',
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  description: {
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: '500',
+    color: '#7A838B',
+    marginTop: 16,
+    width: 240,
+  },
+  scrollViewContainer: {
+    marginTop: 32,
+  },
+  success: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#2D6A4F',
+    paddingVertical: 16,
+    borderRadius: 8,
+    marginVertical: 8,
+  },
+  failure: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#941B0C',
+    paddingVertical: 16,
+    borderRadius: 8,
+    marginVertical: 8,
+  },
+  dateTimeText: {
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: '500',
+    color: '#fff',
+  },
+});
+
+export default HistoryScreen;
